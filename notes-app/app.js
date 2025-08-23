347acb4c73ef5bc97fb3c095da43512566da38e3
Jane Le <janehtle@gmail.com>
2025-08-23 12:21:51 -0400

    new dir and files added

---
+++ notes-app/app.js
@@ -0,0 +1,55 @@
+// const fs = require("fs");
+
+// fs.writeFileSync("notes.txt", "Jane created this!");
+
+// // Challenge: Append a message (str) to the notes.txt file using .appendFileSync
+// // DO NOT override pre-existing message
+// fs.appendFileSync("notes.txt", " Jane is learning Node.js!");
+// // Make sure to start every new message with a space when appending to an existing str for proper spacing
+
+
+// const add = require("./utils.js");
+
+// const sum = add(4, -2);
+// console.log(sum);
+
+
+// Challenge: Define and use a function in a new file
+// 1. Create a new file: notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes func
+// 4. From app.js, load in and call the func printing message to console
+
+// const validator = require("validator");
+
+// console.log(validator.isURL("https://mead.io")); //Output: true
+// console.log(validator.isEmail("jgmail.com")); // Output: false
+
+// const getNotes = require("./notes"); //no need to add .js at the end
+
+// const notes = getNotes();
+// console.log(notes);
+
+// Challenge: Use chalk library in this project
+// 1. Install latest ver of chalk
+// 2. Load it into app.js
+// 3. Use it to print str "Success!" to the console in green
+// 4. Test your work
+// Bonus: Mess around with other StyleSheet, make text bold and inversed!
+
+// const chalk = require("chalk"); // npm install chalk@4 - this allows use of require() instead of import and w/o type module
+// import chalk from "chalk" //pure ESM-only, can only work if package.json has type module
+
+// console.log(chalk.green("Success!"));
+// console.log(chalk.bold("Hello world!"));
+// console.log(chalk.inverse.bold("Type Script!"));
+// console.log(chalk.red("Type Script!"));
+
+const yargs = require("yargs");
+
+console.log(process.argv);
+console.log(yargs.argv);
+
+
+
+
---
+++ notes-app/notes.js
@@ -0,0 +1,7 @@
+console.log("notes.js");
+
+const getNotes = () => {
+    return "Your notes...";
+}
+
+module.exports = getNotes;
