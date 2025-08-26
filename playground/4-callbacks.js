6e2d7e357fa4750fa2c370968e70784aac8f69c0
Jane Le <janehtle@gmail.com>
2025-08-26 14:28:30 -0400

    update

---
+++ playground/4-callbacks.js
@@ -0,0 +1,79 @@
+setTimeout(() => {
+    console.log("2 seconds are up!");
+}, 2000)
+
+//setTimeout uses the callback pattern
+
+const names = ["Jane", "Mason", "Ramandeep", "Imran"];
+const shortNames = names.filter((name) => {
+    return name.length <= 4;
+});
+
+const geocode = (address, callback) => {
+    
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        }
+
+        callback(data);
+
+    }, 2000)
+    
+}
+
+geocode("Arizona", (data) => {
+    console.log(data);
+});
+
+
+// ---------------
+// User
+// ---------------
+
+// Should not signup a user with email that is already in use
+
+// Should login and provide authentication token
+
+// Should reject me query without authentication
+
+// Should hide emails when fetching list of users
+
+// ---------------
+// Post
+// ---------------
+
+// Should not be able to update another users post
+
+// Should not be able to delete another users post
+
+// Should require authentication to create a post (could add for update and delete too)
+
+// Should fetch published post by id 
+
+// Should fetch own post by id
+
+// Should not fetch draft post from other user
+
+// ---------------
+// Comment
+// ---------------
+
+// Should fetch post comments
+
+// Should create a new comment
+
+// Should not create comment on draft post
+
+// Should update comment
+
+// Should not update another users comment
+
+// Should not delete another users comment
+
+// Should require authentication to create a comment (could add for update and delete too)
+
+add(1, 4, (sum) => {
+    console.log(sum); //Output: 5
+})
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,46 @@
 const request = require("request");
 
-const url = "https://api.weatherstack.com/current?access_key=0be37d0c1fac3196b3c920f7728c495b&query=37.8267,-122.4233&units=f";
+// const url = "https://api.weatherstack.com/current?access_key=0be37d0c1fac3196b3c920f7728c495b&query=37.8267,-122.4233&units=f";
 
 
-//first arg is an options object, outlines what we'd like to do. second arg is a func, runs once response received
-request({ url: url, json: true }, (error, response) => {
-    // const data = JSON.parse(response.body);
-    // console.log(data.current); //data.current used for weatherstack API - data.currently for darksky API which we don't use anymore
-    // console.log(response.body.current);
-    console.log(response.body.current.weather_descriptions[0] + ": It is currently " + response.body.current.temperature + " degrees out. " + "There is " + response.body.current.precip + "% chance of rain.");
-})
+// //first arg is an options object, outlines what we'd like to do. second arg is a func, runs once response received
+// request({ url: url, json: true }, (error, response) => {
+//     // const data = JSON.parse(response.body);
+//     // console.log(data.current); //data.current used for weatherstack API - data.currently for darksky API which we don't use anymore
+//     // console.log(response.body.current);
+
+//     if (error) {
+//         console.log("Unable to connect.");
+//     } else if (response.body.error) {
+//         console.log("Unable to find location.");
+//     } else {
+//         console.log(response.body.current.weather_descriptions[0] + ": It is currently " + response.body.current.temperature + " degrees out. " + "There is " + response.body.current.precip + "% chance of rain.");
+//     }
+// })
 
 
 // GEOCODING
 
+const geocodeURL = "https://api.mapbox.com/search/geocode/v6/forward?q=Los%20Angeles&access_token=pk.eyJ1IjoiamxlMTciLCJhIjoiY21lcmNtd3JwMDVtbTJqcTB5NTVuYjdoMSJ9.oeLdLdniFxB-L8mz06wHIA&limit=1";
 
+request({ url: geocodeURL, json: true }, (error, response) => {
+    // const latitude = response.body.features[0].center[0];
+    // const longitude = response.body.features[0].center[1];
+
+    if (error) {
+        console.log("Unable to connect.");
+    } else if (response.body.features.length === 0) {
+        console.log("Search again.");
+    } else {
+        const feature = response.body.features[0]; //might as well assign it to a variable to make code concise below
+
+        //mapbox is now using geometry.coordinates inside each feature for lat/lon
+        const longitude = feature.geometry.coordinates[0]; //in URL, center was not present consisting of the lat/lon, it was in coordinates
+        const latitude = feature.geometry.coordinates[1];
+
+        console.log(latitude, longitude);
+    }
+})
 
 
 // // ASYNCHRONOUS BASICS
